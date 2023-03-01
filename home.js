bdfd05307bbbbe470118fd19c736eec49d899907
wilsonpaiva1 <wspxxxx@gmail.com>
2023-03-01 17:37:53 -0300

    Nova home finalizadda

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(){
     return nome
 }
+
+function mostraSobrenome(){
+    return sobrenome;
+}
+
+//finalizado sistema da home
--- login.js
+++ login.js
@@ -1,2 +1,5 @@
 //Trabalhando no login
 //finalizando sistema de login
+function login(name,email){
+    return 'Bem vindo';
+}
